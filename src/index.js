e3c7352571dd549bf97355c1d9181e9c11c02a7d
JoseJavega <josejavega@gmail.com>
2025-10-16 00:26:14 +0200

    chore: añadida captura de ruta para servir index.htm y documentos en public/

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 import { tasksRouter } from './routes/tasks.js';
 import { corsMiddleware } from './middlewares/cors.js';
 
@@ -10,8 +12,19 @@ app.use(express.json()); // para acceder a los datos del req.body
 app.use(corsMiddleware()); // accesos autorizador de CORS
 app.disable('x-powered-by'); // desabilita la cabecera de express
 
+// Convierte la URL del módulo actual a ruta de archivo
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// permite a express servir directamente los archivos estáticos de public
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 // TASKS
-app.use('/tasks',tasksRouter);
+app.use('/api/tasks',tasksRouter);
+
+// Ruta general para servir index.html a todas las rutas que no coincidan antes
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT ?? 3000;
 
