a7e5f88e3599ebcb866236698666a93b12a5be80
JoseJavega <josejavega@gmail.com>
2026-01-19 18:31:54 +0100

    refact(categorias): cambiada la ruta de taskCategory a taskCategories

--- public/js/services/TaskCategory.js
+++ public/js/services/TaskCategory.js
@@ -1,77 +1,80 @@
 export class TaskCategoryService {
-  constructor(apiUrl){
-    this.apiClient= `${apiUrl}/taskCategory`;
-  };
+  constructor(apiUrl) {
+    this.apiClient = `${apiUrl}/taskCategories`;
+  }
 
-  async getAll(){
+  async getAll() {
     try {
-      const result= await fetch(this.apiClient);
-      const data= await result.json();
+      const result = await fetch(this.apiClient);
+      const data = await result.json();
       return data;
     } catch (error) {
       return [];
-    };
-  };
+    }
+  }
 
-  async getById(id){
-  try {
-    const result= await fetch(`${this.apiClient}/${id}`);
-    const data= await result.json();
-    return data;
-  } catch (error) {
-    console.log('error:',error);
-    };
-  };
+  async getById(id) {
+    try {
+      const result = await fetch(`${this.apiClient}/${id}`);
+      const data = await result.json();
+      return data;
+    } catch (error) {
+      console.log("error:", error);
+    }
+  }
 
   async save(data) {
-    if (!data) { return };
+    if (!data) {
+      return;
+    }
     try {
       const result = await fetch(`${this.apiClient}`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          name: data
-        })
+          name: data,
+        }),
       });
 
-      const newCategory= await result.json()
+      const newCategory = await result.json();
       return newCategory;
     } catch (error) {
       console.error(error);
-    };
-  };
+    }
+  }
 
-  async update(data){
-    if (!data.id) { return }
+  async update(data) {
+    if (!data.id) {
+      return;
+    }
     try {
       const result = await fetch(`${this.apiClient}/${data.id}`, {
-        method: 'PATCH',
+        method: "PATCH",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          name: data.name
-        })
+          name: data.name,
+        }),
       });
 
-      const updatedCategory= await result.json()
+      const updatedCategory = await result.json();
       return updatedCategory;
     } catch (error) {
       console.log(error);
-    ;}
-  };
+    }
+  }
 
-  async delete(id){
+  async delete(id) {
     try {
-      await fetch(`${this.apiClient}/${id}`,{
-        method: 'DELETE'
+      await fetch(`${this.apiClient}/${id}`, {
+        method: "DELETE",
       });
-     return id;
+      return id;
     } catch (error) {
       throw new Error(error);
-    };
-  };
-
+    }
+  }
 }
