40b78510acc8c941a968f53f1e23fb3357f0a00b
JoseJavega <josejavega@gmail.com>
2026-01-22 01:14:22 +0100

    fix(taskCategoryService): actualizado para que al borrar una categoria, sus tareas asociadas pasen a uncategorized. Closes #16

--- src/services/taskCategory.js
+++ src/services/taskCategory.js
@@ -2,12 +2,11 @@ import { TaskModel } from "../models/task.js";
 import { TaskCategoryModel } from "../models/taskCategory.js";
 
 export class TaskCategoryService {
-  static async deleteCategory(categoryId){
-    const tasks = await TaskModel.getAll({categoryId});
-    // si hay tareas en esa categoria no se borra
-    if (tasks.length > 0) { return null }
-
-    const result = await TaskCategoryModel.delete(categoryId);
-    return result;
+  static async deleteCategory(categoryId) {
+      // buscamos tareas con esa categoría y seteamos categoryId a "uncategorized"
+      await TaskModel.updateMany( "categoryId", categoryId, "uncategorized" );
+      // borramos la categoría
+      const result = await TaskCategoryModel.delete(categoryId);
+      return result !==false;
   }
 }
